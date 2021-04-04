0493a56d20f81776ec9e1f9167eaf8e6950e199f
Sergey <xxsmotur@gmail.com>
2021-04-04 20:26:39 +0500

    Draft for publishing

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/saromanov/rabbitmq-rpc
+
+go 1.16
+
+require github.com/streadway/amqp v1.0.0 // indirect
---
+++ internal/publish/publish.go
@@ -0,0 +1,42 @@
+package publish
+
+import (
+	"fmt"
+
+	"github.com/streadway/amqp"
+	"github.com/pkg/errors"
+)
+type Publish struct {
+	channel *amqp.Channel
+}
+
+// New creates new publisher
+func New(channel *amqp.Channel) (*Publish, error){
+	if channel == nil {
+		return nil, errors.New("channel is not defined")
+	}
+	return &Publish{
+		channel: channel,
+	}, nil
+}
+
+// Do provides sending of the message
+func (p *Publish) Do(queue string, data []byte) error {
+	err := p.channel.Publish(
+		"",
+		queue,
+		false,
+		false,
+		amqp.Publishing{
+			ContentType:   "application/octet-stream",
+			CorrelationId: "mock",
+			ReplyTo:       "test-reply",
+			Body:          data,
+			Expiration:    "1",
+		})
+	if err != nil {
+		return errors.Wrap(err, "unable to send message")
+	}
+	return nil
+}
+
